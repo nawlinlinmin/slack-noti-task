4ae5774684580d8152de1cc5dbb6d9e9a4778c44
naw linlin min <nodmentor1ygn@gmail.com>
2020-12-04 00:11:41 +0630

    send noti

---
+++ sample.rb
@@ -0,0 +1,13 @@
+require "slack-notify"
+require 'clockwork'
+require 'dotenv'
+Dotenv.load
+
+require 'active_support/time'
+
+module Clockwork
+  every(10.seconds,'test') do
+    sample = SlackNotify::Client.new(webhook_url: ENV['WEBHOOK'])
+    sample.notify("Hi")
+  end
+end
